555dea02c243585bc3571dd2a07f04453ba7cde2
AkiraMatthew <akira.dev.math@gmail.com>
2023-10-02 19:15:49 -0300

    inheriting the VRFConsumerBaseV2 and adding the vrfCoordinatorV2

--- contracts/contracts/Raffle.sol
+++ contracts/contracts/Raffle.sol
@@ -9,9 +9,11 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee; // -> price to join the lottery pool
     address payable[] private s_players;
@@ -19,7 +21,12 @@ contract Raffle {
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    /**
+     *
+     * @param vrfCoordinatorV2 the vrfCoordinatorV2 it's the contract
+     * that does the random number verification
+     */
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -33,8 +40,18 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    //function pickRandomWinner(){}
+    function requestRandomWinner() external {
+        // get the random number
+        // when we get it, do something with it
+        // Chainlink VRF it's a 2 transaction process
+    }
 
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
